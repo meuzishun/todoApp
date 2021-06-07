620d530bdf41d37037d85603d4474bb98a9de95a
Andrew Smith <meuzishun@gmail.com>
2021-06-07 10:52:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.querySelector('#text-field');
+const addItemBtn = document.querySelector('#submit-btn');
+const clearListBtn = document.querySelector('#clear-btn');
+const list = document.querySelector('#list');
+
+function getInput() {
+    let value = input.value;
+    input.focus();
+    input.value = '';
+    return value;
+}
+
+function createMarkup(value) {
+    let listElem = document.createElement('li');
+    let label = document.createElement('span');
+    let removeBtn = document.createElement('button');
+
+    label.innerHTML = value;
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.addEventListener('click', removeItem);
+
+    listElem.appendChild(label);
+    listElem.appendChild(removeBtn);
+    list.appendChild(listElem);
+}
+
+function removeItem(evt) {
+    evt.target.parentElement.remove();
+}
+
+function clearList() {
+    Array.from(list.children).forEach(item => {
+        item.remove();
+    });
+}
+
+addItemBtn.addEventListener('click', function() {
+    createMarkup(getInput());
+});
+
+clearListBtn.addEventListener('click', clearList);
